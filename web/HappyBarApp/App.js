4babd409635b370f60cbca25ea32db13492cd757
EasonAndLily <15342349100@163.com>
2018-03-09 23:20:24 +0800

    Set up a demo for react-web to access server

--- web/HappyBarApp/App.js
+++ web/HappyBarApp/App.js
@@ -9,45 +9,88 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView
 } from 'react-native';
 
 import APIS from './service/common/apis.js';
 import FetchUtils from './service/common/fetchUtils.js';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
     super(props);
-    this.state = {comedies: []};
+    
+    this.getData = this.getData.bind(this);
+
+    let ds = new ListView.DataSource({
+        rowHasChanged: (oldRow, newRow) => {
+            oldRow !== newRow;
+        },
+    });
+    this.state = {dataSource: ds};
+  }
+
+  componentWillMount() {
+      this.getData();
+  }
+
+  getData() {
     let url = APIS.getAllComedies;
-    let that = this;
+    var _this = this;
     FetchUtils.getRequest(url, function(comedies) {
       console.log(comedies);
-      that.setState({comedies: comedies});
+      let ds = new ListView.DataSource({
+          rowHasChanged: (oldRow, newRow) => {
+              oldRow !== newRow;
+          },
+      });
+      _this.setState({dataSource: ds.cloneWithRows(comedies)});
     }, function(err) {
       alert(err);
     });
   }
 
+  renderRow(comedy) {
+      return (
+          <View style={styles.movieItem}>
+              <View style={styles.movieDiscription}>
+                <Text style={styles.title}>
+                  {comedy.title}
+                </Text>
+                <Text style={styles.year}>
+                  {comedy.playTime}
+                </Text>
+              </View>
+          </View>
+      );
+  }
+
+  renderHeader() {
+      return (
+          <View style={styles.header}>
+              <Text style={styles.headerText}>
+                  Comedies List
+              </Text>
+              <View style={styles.headerLine}></View>
+          </View>
+      );
+  }
+
+  renderSeparator(sectionID:number, rowID:number) {
+      return (
+          <View style={styles.separator} key={sectionID+rowID}>
+          </View>
+      );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-        </Text>
+        <ListView dataSource={this.state.dataSource} renderRow={this.renderRow}
+             style={styles.ListContainer} renderHeader={this.renderHeader}
+             renderSeparator={this.renderSeparator} initialListSize={10}>
+        </ListView>
       </View>
     );
   }
@@ -60,14 +103,54 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  ListContainer: {
+      flex: 1,
+      marginTop: 25
+  },
+  movieItem: {
+    flexDirection: "row",
+    padding: 5,
+    alignItems: "center",
+    backgroundColor: '#F5F5F5'
+  },
+  poster: {
+    width: 53,
+    height: 81,
+    backgroundColor: 'gray'
+  },
+  movieDiscription: {
+    marginLeft: 10,
+    flex: 1
+  },
+  title: {
     fontSize: 20,
+    marginTop: 3,
+    marginBottom: 3,
     textAlign: 'center',
-    margin: 10,
+    fontWeight: 'bold'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  year: {
+    fontSize: 12,
+    marginBottom: 3,
+    textAlign: 'center'
   },
+  header: {
+      height: 50,
+      backgroundColor: '#F5F5F5'
+  },
+  headerText: {
+      flex: 1,
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      lineHeight: 44,
+  },
+  headerLine: {
+      height: 1,
+      backgroundColor: "#ccc",
+  },
+  separator: {
+      height: 1,
+      backgroundColor: "#ccc",
+  }
 });
